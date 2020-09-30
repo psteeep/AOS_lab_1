0ac9d31ea6f2ee475e4b55869c0e20e12f97536c
psteeep <psteeep@users.noreply.github.com>
2020-09-30 19:52:11 +0300

    corrected code corrected output

--- Source.cpp
+++ Source.cpp
@@ -4,10 +4,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include<iomanip>
 
 template <typename testype>
-double adding(long long iter) {
+double adding(long iter) {
 	testype a1 = 9041, a2 = 9349, a3 = 9281, b1 = 9421, b2 = 9133, b3 = 8783, c1 = 8719, c2 = 8669, c3 = 8467;
 	double tact(0);
 	long double start_time = clock();
@@ -26,7 +26,7 @@ double adding(long long iter) {
 }
 
 template <typename testype>
-double subtraction(long long iter) {
+double subtraction(long iter) {
 	testype a1 = 9041, a2 = 9349, a3 = 9281, b1 = 9421, b2 = 9133, b3 = 8783, c1 = 8719, c2 = 8669, c3 = 8467;
 	double tact(0);
 	long double start_time = clock();
@@ -44,7 +44,7 @@ double subtraction(long long iter) {
 }
 
 template <typename testype>
-double multiplication(long long iter) {
+double multiplication(long iter) {
 	testype a1 = 9041, a2 = 9349, a3 = 9281, b1 = 9421, b2 = 9133, b3 = 8783, c1 = 8719, c2 = 8669, c3 = 8467;
 	double tact(0);
 	long double start_time = clock();
@@ -62,7 +62,7 @@ double multiplication(long long iter) {
 }
 
 template <typename testype>
-double division(long long iter) {
+double division(long iter) {
 	testype a1 = 9041, a2 = 9349, a3 = 9281, b1 = 9421, b2 = 9133, b3 = 8783, c1 = 8719, c2 = 8669, c3 = 8467;
 	double tact(0);
 	long double start_time = clock();
@@ -93,31 +93,35 @@ double max_tact(std::vector<std::vector<double>> tact) {
 
 int main() {
 
-	long long N = 10e6; // iterations
+	long iter = 10e6; // iterations
 
-	std::vector<std::vector<double>> tact{ {adding<int>(N),adding<long>(N * 10),adding<char>(N * 10), adding<float>(N * 10), adding<double>(N * 10)},
-		{subtraction<int>(N * 10),subtraction<long>(N * 10),subtraction<char>(N * 10),subtraction<float>(N * 10),
-								subtraction<double>(N * 10)},{multiplication<int>(N),multiplication<long>(N),multiplication<char>(N),
-								multiplication<float>(N),multiplication<double>(N)},{division<int>(N),
-								division<long>(N),division<char>(N),division<float>(N),division<double>(N)} };
+	std::vector<std::vector<double>> tact{ {adding<int>(iter),adding<long>(iter * 10),adding<char>(iter * 10), adding<float>(iter * 10), adding<double>(iter * 10)},
+		{subtraction<int>(iter * 10),subtraction<long>(iter * 10),subtraction<char>(iter * 10),subtraction<float>(iter * 10),
+								subtraction<double>(iter * 10)},{multiplication<int>(iter),multiplication<long>(iter),multiplication<char>(iter),
+								multiplication<float>(iter),multiplication<double>(iter)},{division<int>(iter),
+								division<long>(iter),division<char>(iter),division<float>(iter),division<double>(iter)} };
 
 	std::string oper[4] = { "+", "-", "*", "/" };
 	std::string types[5] = { "int   ", "long  ", "char  ", "float ", "double" };
 
 	double max_tact1 = max_tact(tact);
 
-	for (int j = 0; j < 5; ++j) {
-		for (int i = 0; i < 4; ++i) {
-			std::cout << oper[i] << "  " << types[j] << "  " << tact[i][j] << "  ";
-			double percent = (tact[i][j] / max_tact1) * 100;
-			for (int k = 0; k < int(percent); ++k) std::cout << 'X';
-			for (int k = 0; k < 100 - int(percent); ++k) std::cout << ' ';
-			std::cout << "  " << percent << "%\n";
+	for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < 4; i++) {
+			std::cout << oper[i] << "  " << types[j] << "  " << std::setw(9) << tact[i][j] << " ";
+			double frequencies = (tact[i][j] / max_tact1) * 100;
+			for (int k = 0; k < int(frequencies); k++) {
+				std::cout << 'X';
+			}
+			for (int k = 0; k < 100 - int(frequencies); k++){
+				std::cout << ' ';
 		}
-		std::cout << '\n';
+			std::cout.setf(std::ios::fixed);
+			std::cout.precision(0);
+			std::cout << "  " <<frequencies << "%" << std::endl;
+		}
 	}
 
 
-	
 	return 0;
 }
